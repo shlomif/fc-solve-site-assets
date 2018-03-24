7c3cd514608792cf393d3287da3032a7f25b48b0
Shlomi Fish <shlomif@shlomifish.org>
2018-03-24 23:41:38 +0300

    Add a program to generate msg.

---
+++ fc-solve-site-assets/gen_mswin_commit_msg.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Added the Win{32,64} $(perl -e 'shift =~ /([0-9]+\.[0-9]+\.[0-9]+)/ and print $1' "$@") binaries"
+echo
+for cmd in sha256sum sha512sum
+do
+    l="$cmd $@"
+    echo "\$ $l"
+    eval "$l"
+done
