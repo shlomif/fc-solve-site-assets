bec13c8dac3481704881f378c752742445f5df2b
Shlomi Fish <shlomif@shlomifish.org>
2021-10-11 07:48:27 +0300

    port to Fedora v34

--- fc-solve-site-assets/gen_mswin_commit_msg.bash
+++ fc-solve-site-assets/gen_mswin_commit_msg.bash
@@ -2,7 +2,8 @@
 
 echo "Added the Win{32,64} $(perl -e 'shift =~ /([0-9]+\.[0-9]+\.[0-9]+)/ and print $1' "$@") binaries"
 echo
-for cmd in sha256sum sha512sum sha3-512sum
+# for cmd in sha256sum sha512sum sha3-512sum
+for cmd in sha256sum sha512sum "sha3sum --algorithm 512"
 do
     l="$cmd $@"
     echo "\$ $l"
